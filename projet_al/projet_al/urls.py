943ee6bf6dd7298cdab2ae6abf15bc379454c866
boubou laye <bndiaye0799@gmail.com>
2021-10-15 14:28:53 +0000

    links

--- projet_al/projet_al/urls.py
+++ projet_al/projet_al/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+path('accounts/', include('django.contrib.auth.urls')),
 from App.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name='home'),
     path('signup', signup, name='signup'),
-    path('login', login, name='login')
+    path('accounts/', include('django.contrib.auth.urls')),
+
 ]
