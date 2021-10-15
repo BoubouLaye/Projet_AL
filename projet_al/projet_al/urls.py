f18822db04bf4ceedae664a251f40d9020296c34
KillerObee <mansourkb@hotmail.com>
2021-10-15 03:50:13 +0000

    log in

--- projet_al/projet_al/urls.py
+++ projet_al/projet_al/urls.py
@@ -5,5 +5,6 @@ from App.views import *
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name='home'),
-    path('signup', signup, name='signup')
+    path('signup', signup, name='signup'),
+    path('login', login, name='login')
 ]
